7d5080fa1689fb9664fe3bcb57ffb6eaeea3af79
User <User@DESKTOP-ESP2JT1>
2022-11-08 14:33:26 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace maxNum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // read n
+
+            // for loop - read n numbers
+                // check if is greater the maxNum
+                     // if greater > save new max
+
+            // 3
+            int n = int.Parse(Console.ReadLine());
+            int max = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n-1; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (num > max)
+                {
+                    max = num;
+                }
+            }
+            Console.WriteLine($"Max number: {max}"); 
+            
+            
+
+            
+            
+        }
+    }
+}
